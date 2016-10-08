df27028cd9dae57006552fb61cf1b8f0c81120b9
Stuart Brown <stuart.brown@cohaesus.co.uk>
2016-10-08 17:48:42 +0100

    applied BEM principles to navbar

--- _js/navbar.js
+++ _js/navbar.js
@@ -2,7 +2,7 @@ $(function(){
 	'use strict';
 
 	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-		var navbar = document.getElementsByClassName("nav")[0];
+		var navbar = document.getElementsByClassName("c-nav")[0];
 		$('#js-icon').on( "click", function() {
 	    	navbar.classList.toggle("responsive");
 		});
