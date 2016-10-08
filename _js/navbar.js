a71b74e14aba8bce01656b543fc3b364d71621f0
Stuart Brown <stuart.brown@cohaesus.co.uk>
2016-10-08 16:10:33 +0100

    replaced old inuit.css framework with newer version

--- _js/navbar.js
+++ _js/navbar.js
@@ -2,8 +2,8 @@ $(function(){
 	'use strict';
 
 	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-		var navbar = document.getElementsByClassName("navi")[0];
-		$('#nav--icon').on( "click", function() {
+		var navbar = document.getElementsByClassName("nav")[0];
+		$('#js-icon').on( "click", function() {
 	    	navbar.classList.toggle("responsive");
 		});
 
