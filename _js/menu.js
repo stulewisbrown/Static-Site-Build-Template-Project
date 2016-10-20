5499e13b4f34aadf70f066104612f6acf47313a3
Stuart Brown <stuart.brown@cohaesus.co.uk>
2016-10-20 20:16:04 +0100

    completely refactored all components as partials

--- _js/menu.js
+++ _js/menu.js
@@ -4,19 +4,15 @@ $(document).ready(function() {
         menutoggle = $('.c-menu-toggle'),
         menuSelector;
 
-
     menutoggle.on("click", function(e) {
         var menuSelect = getMenuSelector(this);
-       
-            var menuFound = $(".c-menu").find("[data-menu-selector='" + menuSelect + "']")
-            $(menuFound[0]).toggleClass('active');
-            menutoggle.toggleClass('active');
-            return false;
-        
+        var menuFound = $(".c-menu").find("[data-menu-selector='" + menuSelect + "']");
+        $(menuFound[0]).toggleClass('active');
+        menutoggle.toggleClass('active');
+        return false;
     });
 
-    var getMenuSelector = function(e){
-
-    	return $(e).data("menu-selector");
-    }
+    var getMenuSelector = function(e) {
+        return $(e).data("menu-selector");
+    };
 });
